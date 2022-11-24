67bab4f667c4b9f61dcbb1942e3cd918940c0de6
Valentin <valentin.monteiro@orange.fr>
2022-11-24 15:15:20 +0100

    Ajout de la bio

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import SculptureGrid from "../components/SculptureGrid/SculptureGrid"
+import Bio from "../components/Bio/Bio"
 
 export default function Home() {
   return (
     <div>
       <SculptureGrid />
+      <Bio />
     </div>
   )
 }
