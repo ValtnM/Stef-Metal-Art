ad59b4ef25b3e50e14067242017a1849704bf8b2
Valentin <valentin.monteiro@orange.fr>
2023-02-01 15:57:58 +0100

    Création de la page de contact

--- components/Foot/Foot.tsx
+++ components/Foot/Foot.tsx
@@ -26,9 +26,9 @@ export default function Foot() {
           <a href="#">
             <li>Liens</li>
           </a>
-          <a href="#">
+          <Link href="/contact">
             <li>Contact</li>
-          </a>
+          </Link>
         </ul>
         <div className={styles.copyright}>
           <FaRegCopyright className="copyright-icon" />
---
+++ pages/contact.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import styles from "../styles/Contact.module.scss";
+
+export default function contact() {
+  return (
+    <div className={styles.contact}>
+      <h2>Me contacter</h2>
+      <form action="">
+        <div className={styles.name}>
+          <div className={styles.firstname}>
+            <label htmlFor="firstname">Prénom</label>
+            <input type="text" id="firstname" />
+          </div>
+          <div className={styles.lastname}>
+            <label htmlFor="lastname">Nom</label>
+            <input type="text" id="lastname" />
+          </div>
+        </div>
+        <label htmlFor="email">Email</label>
+        <input type="email" id="email" />
+        <label htmlFor="firstname">Message</label>
+        <textarea rows={10} id="firstname" />
+        <button>Envoyer</button>
+      </form>
+    </div>
+  );
+}
